ab3a2405872b680b5ab352a255369e28b3750640
IvanRivera771 <47260927+IvanRivera771@users.noreply.github.com>
2021-02-14 10:19:04 -0600

    forma del while

---
+++ src/empezarwhile/Empezarwhile.java
@@ -0,0 +1,17 @@
+package empezarwhile;
+
+public class Empezarwhile {
+
+    public static void main(String[] args) {
+        //BUCLE WHILE
+        int i = 10;
+
+        while (i>=1) {
+            System.out.println(i);
+            i--; //== i=i+1;
+
+        }
+
+    }
+
+}
